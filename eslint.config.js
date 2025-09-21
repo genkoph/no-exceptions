13cb67370cf978df8ee9710cb6d7668c611d08cd
Kristiyan Genkov <genkoph@protonmail.com>
2025-09-21 21:35:00 +0300

    create Main github workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default defineConfig([
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-  },
-  {
-    ...tseslint.configs.recommended,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     rules: {
-      ...tseslint.configs.recommended.rules,
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
   eslintConfigPrettier,
-]);
+];
